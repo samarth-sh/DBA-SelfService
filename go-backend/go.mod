b0032bea503ee6cf6fc8fe117d6b4e1dc3bdede7
samarth-sh <Samarth.Hiremath@angelbroking.com>
2024-09-16 15:31:46 +0530

    Added confirmation email configuration

--- go-backend/go.mod
+++ go-backend/go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/denisenkom/go-mssqldb v0.12.3
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/joho/godotenv v1.5.1
--- go-backend/main.go
+++ go-backend/main.go
@@ -7,6 +7,7 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"net/smtp"
 	"os"
 	"path/filepath"
 	"sync"
@@ -223,7 +224,27 @@ func checkOldPassword(username, serverIP, oldPassword, database string) (bool, e
 	return true, nil
 }
 
+func sendConfirmationEmail(to, username string) error {
+	from := os.Getenv("SMTP_FROM")
+	password := os.Getenv("SMTP_PASSWORD")
+	smtpServer := os.Getenv("SMTP_SERVER")
+	smtpPort := os.Getenv("SMTP_PORT")
 
+	auth := smtp.PlainAuth("", from, password, smtpServer)
+
+	subject := "DBA Login Password Reset Successful"
+	body := fmt.Sprintf("Hello %s,\n\nYour password has been successfully reset.\n\nBest regards,\nYour Company", username)
+	message := []byte(fmt.Sprintf("Subject: %s\r\n\r\n%s", subject, body))
+
+	addr := fmt.Sprintf("%s:%s", smtpServer, smtpPort)
+	err := smtp.SendMail(addr, auth, from, []string{to}, message)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Confirmation email sent to %s", to)
+	return nil
+}
 func findRelatedServers(msdb *sql.DB, serverIP string) ([]string, error) {
 	query := "EXEC FindRelatedServers @ServerIP=?"
 	rows, err := msdb.Query(query, serverIP)
@@ -359,10 +380,16 @@ func updatePassword(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sendSuccessResponse(w, "Password updated successfully")
+	// send email to the user once the password is updated
+	err = sendConfirmationEmail(request.Email, request.Username)
+		if err != nil {
+			log.Printf("Failed to send confirmation email: %v", err)
+		}
 
 	log.Printf("Password updated successfully for user: %s", request.Username)
 }
 }
+
 func logPasswordUpdate(username, serverIP, requestType, requestStatus, message string) {
 	_, err := db.Exec("CALL log_updates($1, $2, $3, $4, $5)", username, serverIP, requestType, requestStatus, message)
 	if err != nil {
